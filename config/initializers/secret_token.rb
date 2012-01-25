eb17cbafcd2795c5da0b9a6ab050c7217b5c4ab4
Gabriel Lim <gabriel@pyrks.com>
2012-01-26 06:40:06 +0800

    Hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteseer::Application.config.secret_token = 'f8ffd41b96f5252b48997654048ec86ce46c063ba377adb23e495346deccd95399f640655e6faeeb0b9ce237b0922779d79537f36601d75be011bcbaca284afe'
